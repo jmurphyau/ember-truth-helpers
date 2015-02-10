c273b829c9f1942e5c931615ea0e02c8645eb8de
James Murphy <jamesmurphyau@me.com>
2015-02-10 13:51:29 +1100

    Support for the latest version of Ember
    registerHelper is now private as _registerHelper

--- addon/utils/register-helper.js
+++ addon/utils/register-helper.js
@@ -1,9 +1,29 @@
 import Ember from 'ember';
 
+function registerHelperIteration1(name, helperFunction) {
+	//earlier versions of ember with htmlbars used this
+	Ember.HTMLBars.helpers[name] = Ember.HTMLBars.makeBoundHelper(helperFunction);
+}
+
+function registerHelperIteration2(name, helperFunction) {
+	//registerHelper has been made private as _registerHelper
+	//this is kept here if anyone is using it
+	Ember.HTMLBars.registerHelper(name, Ember.HTMLBars.makeBoundHelper(helperFunction));
+}
+
+function registerHelperIteration3(name, helperFunction) {
+	//latest versin of ember uses this
+	Ember.HTMLBars._registerHelper(name, Ember.HTMLBars.makeBoundHelper(helperFunction));	
+}
+
 export function registerHelper(name, helperFunction) {
 	if (Ember.HTMLBars._registerHelper) {
-		Ember.HTMLBars.helpers[name] = Ember.HTMLBars.makeBoundHelper(helperFunction);
+		if (Ember.HTMLBars.helpers) {
+			registerHelperIteration1(name, helperFunction);
+		} else {
+			registerHelperIteration3(name, helperFunction);
+		}
 	} else if (Ember.HTMLBars.registerHelper) {
-		Ember.HTMLBars.registerHelper(name, Ember.HTMLBars.makeBoundHelper(helperFunction));
+		registerHelperIteration2(name, helperFunction);
 	}
 }
