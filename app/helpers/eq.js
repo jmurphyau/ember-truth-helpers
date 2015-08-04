61db1c942fab84c48460fc2e1b1453de131d30cc
pzubkou <pzubkou@thumbtack.net>
2015-08-04 12:00:05 +0300

    Reference correct paths for equal and not-equal helpers

--- app/helpers/eq.js
+++ app/helpers/eq.js
@@ -1,4 +1,4 @@
 import Ember from 'ember';
-import { equalHelper } from 'ember-truth-helpers/helpers/eq';
+import { equalHelper } from 'ember-truth-helpers/helpers/equal';
 
 export default Ember.Helper.helper(equalHelper);
