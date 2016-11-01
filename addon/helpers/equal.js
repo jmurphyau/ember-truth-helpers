674f31aea5844faace2b0c33a47bbed912fdefb6
Taras Mankovski <tarasm@gmail.com>
2016-11-01 08:27:24 -0700

    Revert "Using isEqual for equal helper"
    This reverts commit 791751fea4e6814b1b2e7267c126ffe6ff084e18.

--- addon/helpers/equal.js
+++ addon/helpers/equal.js
@@ -1,6 +1,3 @@
-import Ember from 'ember';
-const { isEqual } = Ember;
-
-export function equalHelper([a, b]) {
-  return isEqual(a, b) || isEqual(b, a);
+export function equalHelper(params) {
+  return params[0] === params[1];
 }
