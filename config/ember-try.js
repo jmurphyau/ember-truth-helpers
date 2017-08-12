b4a32b9a81858c917cb2d471ef45e0f8ee653395
toran billups <toranb@gmail.com>
2017-08-12 16:09:58 -0500

    [TRAVIS]: added ember 1.13.13 and 2.4LTS to ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,6 +2,38 @@
 module.exports = {
   scenarios: [
     {
+      name: 'ember-1.13.13',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#1.13.13'
+        },
+        resolutions: {
+          'ember': '1.13.13'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.4',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-4'
+        },
+        resolutions: {
+          'ember': 'lts-2-4'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
