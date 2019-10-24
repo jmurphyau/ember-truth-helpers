1f8d7b7cdaa6712662fbc8415e2706a1f2ab6390
Esteban Arango Medina <marranoparael31@gmail.com>
2019-10-24 09:44:03 -0500

    assert expect to gte-test

--- tests/unit/helpers/gte-test.js
+++ tests/unit/helpers/gte-test.js
@@ -7,24 +7,28 @@ module('helper:gte', function(hooks) {
   setupRenderingTest(hooks);
 
   test('boolean values', async function(assert) {
+    assert.expect(1);
     await render(hbs`[{{gte true true}}] [{{gte true false}}] [{{gte false true}}] [{{gte false false}}]`);
 
     assert.equal(find('*').textContent, '[true] [true] [false] [true]', 'value should be "[false] [true] [false] [true]"');
   });
 
   test('integer values', async function(assert) {
+    assert.expect(1);
     await render(hbs`[{{gte 1 1}}] [{{gte 1 0}}] [{{gte 0 1}}] [{{gte 0 0}}]`);
 
     assert.equal(find('*').textContent, '[true] [true] [false] [true]', 'value should be "[true] [true] [false] [true]"');
   });
 
   test('decimal values', async function(assert) {
+    assert.expect(1);
     await render(hbs`[{{gte 19.2 19.2}}] [{{gte 19.2 3.55}}] [{{gte 3.55 19.2}}] [{{gte 3.55 3.55}}]`);
 
     assert.equal(find('*').textContent, '[true] [true] [false] [true]', 'value should be "[true] [true] [false] [true]"');
   });
 
   test('integers in strings 1', async function(assert) {
+    assert.expect(1);
     await render(
       hbs`[{{gte '1' '1' forceNumber=true}}] [{{gte '1' '0' forceNumber=true}}] [{{gte '0' '1' forceNumber=true}}] [{{gte '0' '0' forceNumber=true}}]`
     );
@@ -33,6 +37,7 @@ module('helper:gte', function(hooks) {
   });
 
   test('integers in strings 2', async function(assert) {
+    assert.expect(1);
     await render(
       hbs`[{{gte '102' '102' forceNumber=true}}] [{{gte '102' '98' forceNumber=true}}] [{{gte '98' '102' forceNumber=true}}] [{{gte '98' '98' forceNumber=true}}]`
     );
@@ -41,6 +46,7 @@ module('helper:gte', function(hooks) {
   });
 
   test('decimals in strings', async function(assert) {
+    assert.expect(1);
     await render(
       hbs`[{{gte '19.2' '19.2' forceNumber=true}}] [{{gte '19.2' '3.55' forceNumber=true}}] [{{gte '3.55' '19.2' forceNumber=true}}] [{{gte '3.55' '3.55' forceNumber=true}}]`
     );
