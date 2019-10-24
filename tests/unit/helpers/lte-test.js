fb0dfce7dd17b248b7ab97671b4667290c9055e8
Esteban Arango Medina <marranoparael31@gmail.com>
2019-10-24 09:47:29 -0500

    assert expect to lte-test

--- tests/unit/helpers/lte-test.js
+++ tests/unit/helpers/lte-test.js
@@ -6,26 +6,29 @@ import hbs from 'htmlbars-inline-precompile';
 module('helper:lte', function(hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
   test('boolean values', async function(assert) {
+    assert.expect(1);
     await render(hbs`[{{lte true true}}] [{{lte true false}}] [{{lte false true}}] [{{lte false false}}]`);
 
     assert.equal(find('*').textContent, '[true] [false] [true] [true]', 'value should be "[false] [false] [true] [true]"');
   });
 
   test('integer values', async function(assert) {
+    assert.expect(1);
     await render(hbs`[{{lte 1 1}}] [{{lte 1 0}}] [{{lte 0 1}}] [{{lte 0 0}}]`);
 
     assert.equal(find('*').textContent, '[true] [false] [true] [true]', 'value should be "[false] [false] [true] [true]"');
   });
 
   test('decimal values', async function(assert) {
+    assert.expect(1);
     await render(hbs`[{{lte 19.2 19.2}}] [{{lte 19.2 3.55}}] [{{lte 3.55 19.2}}] [{{lte 3.55 3.55}}]`);
 
     assert.equal(find('*').textContent, '[true] [false] [true] [true]', 'value should be "[false] [false] [true] [true]"');
   });
 
   test('integers in strings 1', async function(assert) {
+    assert.expect(1);
     await render(
       hbs`[{{lte '1' '1' forceNumber=true}}] [{{lte '1' '0' forceNumber=true}}] [{{lte '0' '1' forceNumber=true}}] [{{lte '0' '0' forceNumber=true}}]`
     );
@@ -34,6 +37,7 @@ module('helper:lte', function(hooks) {
   });
 
   test('integers in strings 2', async function(assert) {
+    assert.expect(1);
     await render(
       hbs`[{{lte '102' '102' forceNumber=true}}] [{{lte '102' '98' forceNumber=true}}] [{{lte '98' '102' forceNumber=true}}] [{{lte '98' '98' forceNumber=true}}]`
     );
@@ -42,6 +46,7 @@ module('helper:lte', function(hooks) {
   });
 
   test('decimals in strings', async function(assert) {
+    assert.expect(1);
     await render(
       hbs`[{{lte '19.2' '19.2' forceNumber=true}}] [{{lte '19.2' '3.55' forceNumber=true}}] [{{lte '3.55' '19.2' forceNumber=true}}] [{{lte '3.55' '3.55' forceNumber=true}}]`
     );
