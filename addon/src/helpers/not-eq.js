ac85219fd4be1c7b7e20512d16ae8ffb63e3c62e
Camille TJHOA <camille.tjhoa@concordnow.com>
2022-05-10 12:23:59 +0000

    Rename some helpers to match ember convention

--- addon/src/helpers/not-eq.js
+++ addon/src/helpers/not-eq.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
-export function notEqualHelper(params) {
+export function notEq(params) {
   return params[0] !== params[1];
 }
 
-export default helper(notEqualHelper);
+export default helper(notEq);
