ab0a5463931cbc4cb258c5c329bd9048d82ed0c4
Sergey Astapov <sergeastapov@gmail.com>
2023-06-21 22:57:56 -0400

    switch from yarn to pnpm

--- packages/modern-test-app/ember-cli-build.js
+++ packages/modern-test-app/ember-cli-build.js
@@ -1,12 +1,11 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const packageJson = require('./package');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     autoImport: {
-      watchDependencies: Object.keys(packageJson.dependencies),
+      watchDependencies: ['ember-truth-helpers'],
     },
   });
 
