6f6b801ed0a549297c6165e154730e09a7650475
Sergey Astapov <v-astas-gri@ae.com>
2023-08-08 21:21:19 -0400

    (fix): infer return type in or helper

--- packages/ember-truth-helpers/src/helpers/or.ts
+++ packages/ember-truth-helpers/src/helpers/or.ts
@@ -2,18 +2,30 @@ import { helper } from '@ember/component/helper';
 import truthConvert from '../utils/truth-convert.ts';
 import type { MaybeTruth } from '../utils/truth-convert.ts';
 
-export interface OrSignature {
-  Args: {
-    Positional: MaybeTruth[];
-  };
-  Return: boolean;
-}
-
-export default helper<OrSignature>((params) => {
+export default helper(<T extends MaybeTruth[]>(params: [...T]) => {
   for (let i = 0, len = params.length; i < len; i++) {
     if (truthConvert(params[i]) === true) {
-      return params[i] as boolean;
+      return params[i] as FirstNonFalsy<T>;
     }
   }
-  return params[params.length - 1] as boolean;
+
+  return params[params.length - 1] as Last<T>;
 });
+
+type FirstNonFalsy<T extends any[]> = T extends [infer K]
+  ? K
+  : T extends [infer A, ...infer R]
+  ? Truthy<A> extends true
+    ? A
+    : FirstNonFalsy<R>
+  : never;
+
+type Last<T extends any[]> = T extends [infer K]
+  ? K
+  : T extends [...infer Q, infer L]
+  ? L
+  : never;
+
+type Truthy<T> = T extends Falsy ? false : true;
+
+type Falsy = false | 0 | '' | null | undefined | { isTruthy: false } | [];
