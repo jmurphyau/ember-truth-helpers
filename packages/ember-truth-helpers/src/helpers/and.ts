ba8ac405d3807360ea1dc7fd8ba19c62cd7a8f23
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-28 14:04:54 -0400

    and

--- packages/ember-truth-helpers/src/helpers/and.ts
+++ packages/ember-truth-helpers/src/helpers/and.ts
@@ -1,4 +1,3 @@
-import { helper } from '@ember/component/helper';
 import truthConvert from '../utils/truth-convert.ts';
 import type { MaybeTruth } from '../utils/truth-convert.ts';
 
@@ -9,11 +8,11 @@ export interface AndSignature {
   Return: boolean;
 }
 
-export default helper<AndSignature>((params) => {
+export default function and<T extends MaybeTruth[]>(...params: [...T]) {
   for (let i = 0, len = params.length; i < len; i++) {
     if (truthConvert(params[i]) === false) {
       return params[i] as boolean;
     }
   }
   return params[params.length - 1] as boolean;
-});
+}
