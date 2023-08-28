4b22c67b4a06f4a1c3d2d17b04b07a32aec14761
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-28 14:52:16 -0400

    not-eq

--- packages/ember-truth-helpers/src/helpers/not-eq.ts
+++ packages/ember-truth-helpers/src/helpers/not-eq.ts
@@ -1,12 +1,3 @@
-import { helper } from '@ember/component/helper';
-
-export interface NotEqSignature {
-  Args: {
-    Positional: [unknown, unknown];
-  };
-  Return: boolean;
+export default function notEq(left: unknown, right: unknown) {
+  return left !== right;
 }
-
-export default helper<NotEqSignature>((params) => {
-  return params[0] !== params[1];
-});
