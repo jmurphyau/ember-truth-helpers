7f24e42dd4f6b044d7c4fce297dd795ebf0827dc
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-28 14:03:10 -0400

    Copy contents from https://github.com/jmurphyau/ember-truth-helpers/pull/185/files

--- packages/ember-truth-helpers/src/helpers/or.ts
+++ packages/ember-truth-helpers/src/helpers/or.ts
@@ -1,19 +1,29 @@
-import { helper } from '@ember/component/helper';
 import truthConvert from '../utils/truth-convert.ts';
 import type { MaybeTruth } from '../utils/truth-convert.ts';
 
-export interface OrSignature {
-  Args: {
-    Positional: MaybeTruth[];
-  };
-  Return: boolean;
-}
-
-export default helper<OrSignature>((params) => {
+export default function or<T extends MaybeTruth[]>(...params: [...T]) {
   for (let i = 0, len = params.length; i < len; i++) {
     if (truthConvert(params[i]) === true) {
-      return params[i] as boolean;
+      return params[i] as FirstNonFalsy<T>;
     }
   }
-  return params[params.length - 1] as boolean;
-});
+  return params[params.length - 1] as Last<T>;
+}
+
+type FirstNonFalsy<T extends any[]> = T extends [infer K]
+  ? K
+  : T extends [infer A, ...infer R]
+  ? Truthy<A> extends true
+    ? A
+    : FirstNonFalsy<R>
+  : never;
+
+type Last<T extends any[]> = T extends [infer K]
+  ? K
+  : T extends [...infer Q, infer L]
+  ? L
+  : never;
+
+type Truthy<T> = T extends Falsy ? false : true;
+
+type Falsy = false | 0 | '' | null | undefined | { isTruthy: false } | [];
