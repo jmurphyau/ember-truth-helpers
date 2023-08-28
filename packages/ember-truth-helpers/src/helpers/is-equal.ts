f0b46e73ec135b88fe20e3651d4d3cf242ebfa08
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-28 14:49:18 -0400

    is-equal

--- packages/ember-truth-helpers/src/helpers/is-equal.ts
+++ packages/ember-truth-helpers/src/helpers/is-equal.ts
@@ -1,13 +1,3 @@
-import { helper } from '@ember/component/helper';
 import { isEqual } from '@ember/utils';
 
-export interface IsEqualSignature {
-  Args: {
-    Positional: [unknown, unknown];
-  };
-  Return: boolean;
-}
-
-export default helper<IsEqualSignature>(([a, b]) => {
-  return isEqual(a, b);
-});
+export default isEqual;
