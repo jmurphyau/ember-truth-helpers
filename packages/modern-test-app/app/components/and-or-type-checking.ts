700ac0c9c75339eb40f9c6baeb33d5513d155d71
Sergey Astapov <v-astas-gri@ae.com>
2023-09-01 22:55:04 -0400

    add Glint check for inferred type of `and` and `or` helpers

---
+++ packages/modern-test-app/app/components/and-or-type-checking.ts
@@ -0,0 +1,19 @@
+import templateOnly from '@ember/component/template-only';
+
+interface Signature {
+  Element: HTMLDivElement;
+  Args: {
+    andArg: object | boolean;
+    orArg: object | boolean;
+  };
+}
+
+const AndOrTypeChecking = templateOnly<Signature>();
+
+export default AndOrTypeChecking;
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    AndOrTypeChecking: typeof AndOrTypeChecking;
+  }
+}
